ad2d528066b1f7310d1fec6f880ba8969a74e63e
Nisan Dalva <nisan.dalva@s.afeka.ac.il>
2021-09-02 17:08:18 +0300

    initial commit

---
+++ src/main/java/com/recipeassignment/RecipeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.recipeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipeassignment/RecipeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
